c658a097c613c000605260e8e023e730fa1f3776
Shivani <shivaniteotia743@gmail.com>
2025-04-09 21:42:35 +0530

    redis queue communication

--- apps/engine/src/RedisManager.ts
+++ apps/engine/src/RedisManager.ts
@@ -14,8 +14,12 @@ export class RedisManager {
             url: "redis://localhost:6379",
         });
 
-        this.pullFromApiQueue.connect().catch(console.error);
-        this.apiPubSub.connect().catch(console.error);
+        this.pullFromApiQueue.connect()
+            .then(() => console.log("Pull From Api Queue: connected to Redis"))
+            .catch(console.error);
+        this.apiPubSub.connect()
+            .then(() => console.log("Api PubSub: connected to Reids"))
+            .catch(console.error);
 
         this.pullFromApiQueue.on("error", (error) => console.error("Pull From Api Queue Error:", error));
         this.apiPubSub.on("error", (error) => console.log("Api Pub Sub Error:", error));
@@ -28,4 +32,7 @@ export class RedisManager {
         return this.instance;
     }
 
+    public getPullFromApiQueueClient(): RedisClientType {
+        return this.pullFromApiQueue;
+    }
 }
--- apps/engine/src/index.ts
+++ apps/engine/src/index.ts
@@ -1,1 +1,24 @@
-console.log("engine folder transpiling successfully");
+import { RedisManager } from "./RedisManager";
+
+async function main() {
+    console.log("engine is running");
+    const apiQueueKey = "messages";
+
+    const redisManager = RedisManager.getInstance();
+    const pullClient = redisManager.getPullFromApiQueueClient();
+
+    while (true) {
+        try {
+            const message = await pullClient.rPop(apiQueueKey);
+            if (!message) {
+
+            } else {
+                console.log(message);
+            }
+        } catch (error) {
+
+        }
+    }
+}
+
+main();
